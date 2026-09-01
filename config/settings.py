2c8c04a82e0f73055c8cb8c797e81c1d2381a146
mytutoeasy <127867158+mytutoeasy@users.noreply.github.com>
2026-09-01 22:33:10 +0100

    Fix Vercel HTTP_HOST ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dev-key"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djapross.vercel.app", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
