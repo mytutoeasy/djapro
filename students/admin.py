f1c2982be7293de3d4f6dc883b47c221ad68dafb
mytutoeasy <127867158+mytutoeasy@users.noreply.github.com>
2026-09-01 16:00:58 +0100

    Register Student in admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your student models here.
+from .models import Student
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "phone", "gender", "created_at")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("gender", "created_at")
